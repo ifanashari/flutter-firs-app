57c218a4c498337267a00f400e840f346eb0c4a0
Moh. Khafidzul Ifan Ashari <moh.ashari@gojek.com>
2024-11-05 19:02:16 +0700

    feat: add custom class

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepPurple, Colors.black],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ), 
+    const MaterialApp(
+      home: Scaffold(backgroundColor: Colors.amber, body: GradientContainer()),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.black],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
