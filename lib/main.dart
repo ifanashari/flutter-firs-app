001aafaa1fa5e181dac1547b16f56e84252428bf
Moh. Khafidzul Ifan Ashari <moh.ashari@gojek.com>
2024-11-05 18:31:38 +0700

    feat: custom text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello world')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.black],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello world',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ), 
+          ),
+        ),
+      ),
+    ),
+  );
 }
